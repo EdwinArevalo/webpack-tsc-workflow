0f580607bc6bf6d1653e86679a8d01892519e86a
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-09-03 01:08:38 -0500

    image minify added

--- webpack.prod.js
+++ webpack.prod.js
@@ -75,7 +75,31 @@ module.exports = {
                             outputPath: 'assets/',
                             useRelativePath: true
                         }
-                    }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        }
+                      },
                 ]
             }
         ]
